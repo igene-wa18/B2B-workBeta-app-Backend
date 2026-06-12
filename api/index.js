eadae38b1bdafcf821883d5333264d26e675d980
igene-wa18 <rameshkumar9693459534@gmail.com>
2026-06-12 17:59:52 +0530

    fixed the login issue

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const notificationRoutes = require('../routes/notifications');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
