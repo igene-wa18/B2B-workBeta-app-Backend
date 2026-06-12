cc8cfbfc867a32f504504855476b0c79d8594c41
igene-wa18 <rameshkumar9693459534@gmail.com>
2026-06-12 16:42:50 +0530

    fixed render port binding

--- api/index.js
+++ api/index.js
@@ -56,7 +56,14 @@ app.use((err, req, res, next) => {
 });
 
 // Connect DB and export app for Vercel
-connectDB().catch(err => console.error('DB connection failed:', err));
-
-module.exports = app;
+const Port = process.env.PORT || 5000;
+connextDb()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Database connection error:', err);
+    });
 
